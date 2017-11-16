6a7e0b247bbb88954098950ece7244a5cc267d7f
ipad_kid <ipadkid358@gmail.com>
2017-11-16 17:18:46 -0500

    Added comments and readMe, version 0.5

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
+// needed for dlopen and dlclose
 #import <dlfcn.h>
-
-@import UIKit;
-@import MobileCoreServices;
+// UIImage, and Foundation
+#import <UIKit/UIKit.h>
+// For linking purposes with Xcode
+#import <MobileCoreServices/MobileCoreServices.h>
 
 @interface LSApplicationProxy
 @property (readonly) NSString *bundleIdentifier;
@@ -16,59 +18,62 @@
 + (UIImage *)_applicationIconImageForBundleIdentifier:(nonnull NSString *)bundleIdentifier format:(int)format scale:(CGFloat)scale;
 @end
 
-NSString *endPath = @"/private/var/mobile/Library/Sinfool";
-NSString *prefsPath = @"/private/var/mobile/Library/Preferences/com.ipadkid.sinfoolrepo.plist";
-NSFileManager *fileManager;
-
-BOOL sameAppSet(NSArray *allAppDirs) {
-    if (![fileManager fileExistsAtPath:endPath]) return NO;
-    if (![[fileManager contentsOfDirectoryAtPath:endPath error:NULL] count]) return NO;
-    if ([allAppDirs isEqualToArray:[NSArray arrayWithContentsOfFile:prefsPath]]) return YES;
-    return NO;
-}
-
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
-        fileManager = NSFileManager.defaultManager;
-        
-        NSArray<LSApplicationProxy *> *appProxies = LSApplicationWorkspace.defaultWorkspace.allInstalledApplications;
-        NSMutableArray<NSString *> *allAppRoots = NSMutableArray.new;
-        for (LSApplicationProxy *appProxy in appProxies) [allAppRoots addObject:appProxy.bundleIdentifier];
-        if (sameAppSet(allAppRoots)) return 0;
-        
+        // some constants
+        NSString *endPath = @"/private/var/mobile/Library/Sinfool";
         CGFloat scale = UIScreen.mainScreen.scale;
-        void *cynject = dlopen("/Library/Frameworks/CydiaSubstrate.framework/Libraries/SubstrateInjection.dylib", RTLD_NOW);
-        NSData *badImage = UIImagePNGRepresentation([UIImage _applicationIconImageForBundleIdentifier:@"" format:1 scale:scale]);
         NSString *png = @"png";
-        
-        [fileManager removeItemAtPath:endPath error:NULL];
-        setuid(501);
-        [allAppRoots writeToFile:prefsPath atomically:YES];
-        [fileManager createDirectoryAtPath:endPath withIntermediateDirectories:NO attributes:NULL error:NULL];
-        
-        CFRunLoopRef runLoop = CFRunLoopGetCurrent();
+        NSFileManager *fileManager = NSFileManager.defaultManager;
+        NSArray<LSApplicationProxy *> *appProxies = LSApplicationWorkspace.defaultWorkspace.allInstalledApplications;
         NSUInteger appCount = appProxies.count;
         
+        // delete entire directory and recreate so we don't have to worry about deleted apps
+        if ([fileManager fileExistsAtPath:endPath]) [fileManager removeItemAtPath:endPath error:NULL];
+        setuid(501); // setting to mobile because permissions for running in terminal default
+        [fileManager createDirectoryAtPath:endPath withIntermediateDirectories:NO attributes:0 error:NULL];
+        
+        // due to theos debug defaults, a lot of tweaks that inject UIKit will log to console, this forwards those to /dev/null
+        FILE *devNull = freopen("/dev/null", "w", stderr);
+        
+        // load Substrate so icons are themed
+        void *cynject = dlopen("/Library/Frameworks/CydiaSubstrate.framework/Libraries/SubstrateInjection.dylib", RTLD_NOW);
+        
+        // get that blank icon image
+        NSData *badImage = UIImagePNGRepresentation([UIImage _applicationIconImageForBundleIdentifier:@"" format:1 scale:scale]);
+        
+        // needed for command line tools which don't have a runloop
+        CFRunLoopRef runLoop = CFRunLoopGetCurrent();
+        
+        // block indicates the variable will be mutated within a block
         __block NSUInteger completionCount = 0;
         for (LSApplicationProxy *appProxy in appProxies) {
             dispatch_async(dispatch_get_global_queue(0, 0), ^{
                 NSString *bundleID = appProxy.bundleIdentifier;
                 NSString *fullDestPath = [[endPath stringByAppendingPathComponent:bundleID] stringByAppendingPathExtension:png];
                 
+                // the data that's going to be written
                 NSData *writeable = UIImagePNGRepresentation([UIImage _applicationIconImageForBundleIdentifier:bundleID format:1 scale:scale]);
+                
+                // check if we got a blank icon
                 BOOL goodWrite = ![writeable isEqualToData:badImage];
                 
                 dispatch_async(dispatch_get_main_queue(), ^{
                     if (goodWrite) [writeable writeToFile:fullDestPath atomically:NO];
                     
                     completionCount++;
+                    
+                    // now we're done, and can run the stuff under CFRunLoopRun()
                     if (completionCount == appCount) CFRunLoopStop(runLoop);
                 });
             });
         }
+        // once this line is run, nothing under it will be run until CFRunLoopStop() is called on this runloop
         CFRunLoopRun();
         
+        // close things opened earlier
         dlclose(cynject);
+        fclose(devNull);
     }
     return 0;
 }
---
+++ readMe.md
@@ -0,0 +1,3 @@
+## Sinfool Repo Icons
+
+Command line tool for iOS that gets all valid app icons and puts them into a centeral location on disk. This utility was built to support the Sinfool Repo Icons package
